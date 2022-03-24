9c06352d9762570d2962072c8ecb0b44cf9327b3
Toluprecious <tolulopeibirongbe@gmail.com>
2022-03-24 05:06:41 -0700

    4-puts.css

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
-#!/bin/bash
- #include <stdio.h>
+!#!/bin/bash
+. #include <stdio.h>
+
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
-
 {
 
 	puts("\"Programming is like building a multilingual puzzle");
@@ -14,5 +14,3 @@ int main(void)
 	return (0);
 
 }
-
-
